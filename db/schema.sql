02421fc409011fc71e8079e8d431fe3b86bc7c61
Minal Kharche <minal.berkeley@gmail.com>
2019-07-08 23:07:12 -0700

    added with database schema.sql, created table and fields

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
 
-CREATE DATABASE burgers_db
+-- Drops the eatdaburgers_db if it exists currently
+DROP DATABASE IF EXISTS eatdaburgers_db;
+-- Creates the "eatdaburgers_db" database
+CREATE DATABASE eatdaburgers_db;
+-- specify that all of the following code will affect eatdaburgers_db
+USE eatdaburgers_db;
 
-use DATABASE burgers_db
-
-CREATE TABLE burgers 
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
