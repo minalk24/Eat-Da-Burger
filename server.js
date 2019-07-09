7f99d71e76a0039014b96561ca7b7432171c6673
Minal Kharche <minal.berkeley@gmail.com>
2019-07-08 23:03:40 -0700

    added server.js inclueded with dependencies, config and routes

---
+++ server.js
@@ -0,0 +1,42 @@
+// ---------------------------------------------------
+// Dependencies
+// --------------------------------------------------- 
+
+var express = require("express");
+// create an Express app
+var app = express();
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3030;
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller");
+
+//CONFIG 
+// --------------------------------------------------- 
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+//Setting handlebar engines 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Setting Routes 
+// --------------------------------------------------- 
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
