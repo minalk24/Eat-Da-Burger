e477e081542038ddea7bb6308cd94ced2e58241b
Minal Kharche <minal.berkeley@gmail.com>
2019-07-08 23:08:09 -0700

    added with database entries in seed.sql

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+USE eatdaburgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Turky');
+INSERT INTO burgers (burger_name) VALUES ('Salmon');
+INSERT INTO burgers (burger_name) VALUES ('Veggi');
+INSERT INTO burgers (burger_name) VALUES ('Avacado');
+
